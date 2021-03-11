ee7898a4bc658e0bfd94c77c819dac41e136db6e
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-03-11 17:45:57 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -5,3 +5,13 @@ variable "prefix" {
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
 }
+
+variable "rgName" {
+  description = " Resource Group Name"
+  default = "do_not_delete_New_RG"
+  }
+
+variable "region"{
+  descriptin = "Region Name"
+  default = "West US"
+  }
